f79230db33dc0b3744567fa794bcad865af015a0
Nicolas Barbotte <nicolas@barbotte.net>
2014-06-12 11:14:36 +0200

    ga

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -2,6 +2,22 @@
 
   'use strict';
 
+  (function (i, s, o, g, r, a, m) {
+    i.GoogleAnalyticsObject = r;
+    i[r] = i[r] || function () {
+      (i[r].q = i[r].q || []).push(arguments);
+    },
+    i[r].l = 1 * new Date();
+    a = s.createElement(o),
+    m = s.getElementsByTagName(o)[0];
+    a.async = 1;
+    a.src = g;
+    m.parentNode.insertBefore(a, m);
+  })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+  ga('create', 'UA-41369793-3', 'barbotte.net');
+  ga('send', 'pageview');
+
   var x, y, line, svg, xAxis, yAxis, path, tempText, now, temp, margin, width, height, confs, conf, socket, myFormatters, tooltip;
 
   // Get page width
